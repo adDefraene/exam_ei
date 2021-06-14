4756677fdfb014f4bfc8149a280f985b270d043f
Adrien Defraene <adriendefraene@gmail.com>
2021-06-14 16:54:57 +0200

    Global tweaking

--- src/Controller/AdminPizzaController.php
+++ src/Controller/AdminPizzaController.php
@@ -43,54 +43,65 @@ class AdminPizzaController extends AbstractController
       * @param EntityManagerInterface $manager
       * @return Response
       */
-      public function add(Request $req, EntityManagerInterface $manager, IngredientRepository $repo): Response
-      {
-            $pizza = new Pizza();
-            $ingredients = $repo->findAll();
+    public function add(Request $req, EntityManagerInterface $manager, IngredientRepository $repo): Response
+    {
+        // Init the object
+        $pizza = new Pizza();
+        // Get the ingredients to give them for the js treatment : get a select with all of them
+        $ingredients = $repo->findAll();
 
-            $form = $this->createForm(AdminPizzaType::class, $pizza);
-            $form->handleRequest($req);
+        // Init the form
+        $form = $this->createForm(AdminPizzaType::class, $pizza);
+        $form->handleRequest($req);
 
-            if($form->isSubmitted() && $form->isValid())
-            {   
-                $file = $form['image']->getData();
-                if(!empty($file)){
-                    $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
-                    $safeFilename = transliterator_transliterate('Any-Latin; Latin-ASCII; [^A-Za-z0-9_] remove; Lower()', $originalFilename);
-                    $newFilename = $safeFilename.'-'.uniqid().'.'.$file->guessExtension();
-                    try{
-                        $file->move(
-                            $this->getParameter('uploads_directory'),
-                            $newFilename
-                        );
-                    }
-                    catch(FileException $e)
-                    {
-                        return $e->getMessage();
-                    }
-
-                    $pizza->setImage($newFilename);
+        // hHen form submitted & OK
+        if($form->isSubmitted() && $form->isValid())
+        {   
+            // Get the image data
+            $file = $form['image']->getData();
+            // If not empty
+            if(!empty($file)){
+                // Get a new image treatment
+                $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = transliterator_transliterate('Any-Latin; Latin-ASCII; [^A-Za-z0-9_] remove; Lower()', $originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$file->guessExtension();
+                try{
+                    $file->move(
+                        $this->getParameter('uploads_directory'),
+                        $newFilename
+                    );
                 }
-                
-                $pizza->setType("CLASSIC");
+                catch(FileException $e)
+                {
+                    return $e->getMessage();
+                }
 
-                $manager->persist($pizza);
-                $manager->flush();
-
-                $this->addFlash(
-                    "success",
-                    "La pizza <strong>{$pizza->getName()}</strong> a bien été ajoutée !"
-                );
-
-                return $this->redirectToRoute('admin_pizza_index');
+                $pizza->setImage($newFilename);
             }
+            
+            // Defacto set its type to "CLASSIC"
+            $pizza->setType("CLASSIC");
 
-            return $this->render('admin/pizza/add.html.twig', [
-                'pizza' => $pizza,
-                'myForm' => $form->createView(),
-                'ingredients' => $ingredients
-            ]);
-      }
+            // Persist & Flush
+            $manager->persist($pizza);
+            $manager->flush();
+
+            // Flash
+            $this->addFlash(
+                "success",
+                "La pizza <strong>{$pizza->getName()}</strong> a bien été ajoutée !"
+            );
+
+            // Redirect
+            return $this->redirectToRoute('admin_pizza_index');
+        }
+
+        return $this->render('admin/pizza/add.html.twig', [
+            'pizza' => $pizza,
+            'myForm' => $form->createView(),
+            'ingredients' => $ingredients
+        ]);
+    }
     
     /**
       * Edits an pizza
@@ -102,54 +113,68 @@ class AdminPizzaController extends AbstractController
       * @param EntityManagerInterface $manager
       * @return Response
       */
-      public function edit(Pizza $pizza, Request $req, EntityManagerInterface $manager): Response
-      {
-            $form = $this->createForm(AdminPizzaType::class, $pizza);
-            $form->handleRequest($req);
-            $currentFile = $pizza->getImage();
+    public function edit(Pizza $pizza, Request $req, EntityManagerInterface $manager): Response
+    {
+        // Init the form
+        $form = $this->createForm(AdminPizzaType::class, $pizza);
+        $form->handleRequest($req);
 
-            if($form->isSubmitted() && $form->isValid())
-            {   
-                $pizza->setSlug("");
-                $file = $form['image']->getData();
-                if(!empty($file)){
-                    $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
-                    $safeFilename = transliterator_transliterate('Any-Latin; Latin-ASCII; [^A-Za-z0-9_] remove; Lower()', $originalFilename);
-                    $newFilename = $safeFilename.'-'.uniqid().'.'.$file->guessExtension();
-                    try{
-                        $file->move(
-                            $this->getParameter('uploads_directory'),
-                            $newFilename
-                        );
-                    }
-                    catch(FileException $e)
-                    {
-                        return $e->getMessage();
-                    }
+        // Get current image ICO no changes for the image
+        $currentFile = $pizza->getImage();
 
-                    $pizza->setImage($newFilename);
-                }else{
-                    if(!empty($currentFile)){
-                        $pizza->setImage($currentFile);
-                    }
+        // If form submitted & OK
+        if($form->isSubmitted() && $form->isValid())
+        {   
+            // Resets the slug
+            $pizza->setSlug("");
+
+            // Get the image data
+            $file = $form['image']->getData();
+            // If not empty
+            if(!empty($file)){
+                // New IMG treatment
+                $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = transliterator_transliterate('Any-Latin; Latin-ASCII; [^A-Za-z0-9_] remove; Lower()', $originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$file->guessExtension();
+                try{
+                    $file->move(
+                        $this->getParameter('uploads_directory'),
+                        $newFilename
+                    );
+                }
+                catch(FileException $e)
+                {
+                    return $e->getMessage();
                 }
 
-                $manager->persist($pizza);
-                $manager->flush();
-
-                $this->addFlash(
-                    "success",
-                    "La pizza <strong>{$pizza->getName()}</strong> a bien été modifiée !"
-                );
-
-                return $this->redirectToRoute('admin_pizza_index');
+                $pizza->setImage($newFilename);
+            }else{
+                // If not images provided
+                if(!empty($currentFile)){
+                    // Resets it with the old image
+                    $pizza->setImage($currentFile);
+                }
             }
 
-            return $this->render('admin/pizza/edit.html.twig', [
-                'pizza' => $pizza,
-                'myForm' => $form->createView(),
-            ]);
-      }
+            // Persist & Flush
+            $manager->persist($pizza);
+            $manager->flush();
+
+            // Flash
+            $this->addFlash(
+                "success",
+                "La pizza <strong>{$pizza->getName()}</strong> a bien été modifiée !"
+            );
+
+            // Redirect
+            return $this->redirectToRoute('admin_pizza_index');
+        }
+
+        return $this->render('admin/pizza/edit.html.twig', [
+            'pizza' => $pizza,
+            'myForm' => $form->createView(),
+        ]);
+    }
     
     /**
       * Selects the Pizzas PROMOS
@@ -161,39 +186,57 @@ class AdminPizzaController extends AbstractController
       * @param EntityManagerInterface $manager
       * @return Response
       */
-      public function promos(Request $req, EntityManagerInterface $manager, PizzaRepository $repo): Response
-      {
-            $pizzas = $repo->findAll();   
+    public function promos(Request $req, EntityManagerInterface $manager, PizzaRepository $repo): Response
+    {
+        // Get all the pizzas, in order to generate their select tag
+        $pizzas = $repo->findAll();   
 
-            $form = $this->createForm(AdminPromoType::class);
-            $form->handleRequest($req);
+        // Init the form
+        $form = $this->createForm(AdminPromoType::class);
+        $form->handleRequest($req);
 
-            if($form->isSubmitted() && $form->isValid()){
-                $pizzasToReset = $repo->getSpecialPizzas("PROMO", 3);
-                foreach($pizzasToReset as $pizzaToReset){
-                    $pizzaToReset->setType("CLASSIC");
-                    $manager->persist($pizzaToReset);
-                }
-                $manager->flush();
-                $pizzaToResetJson = json_decode($form->getData()['pizzasToPromo']);
-                foreach($pizzaToResetJson as $pizzasId){
-                    $pizzaToSetAgain = $repo->findPizzaById($pizzasId);
-                    $pizzaToSetAgain->setType("PROMO");
-                    $manager->persist($pizzaToSetAgain);
-                    $manager->flush();
-                }
+        // If Form submitted & OK
+        if($form->isSubmitted() && $form->isValid()){
+            // Reset the pizza's type PROMO back to CLASSIC
+            // Get the pizza in promo
+            $pizzasToReset = $repo->getSpecialPizzas("PROMO", 3);
+            // For each one...
+            foreach($pizzasToReset as $pizzaToReset){
+                // Set them back to "CLASSIC"
+                $pizzaToReset->setType("CLASSIC");
+                // Persist it
+                $manager->persist($pizzaToReset);
             }
-        
-            $this->addFlash(
-                "success",
-                "Les pizzas en promotions ont bien été modifiées !"
-            );
+            // Flush it once
+            $manager->flush();
 
-            return $this->render('admin/pizza/promo.html.twig', [
-                'myForm' => $form->createView(),
-                'pizzas' => $pizzas,
-            ]);
-      }
+            // Sets the choosen ones to "PROMO"
+            // Decode first the sent array
+            $pizzaToResetJson = json_decode($form->getData()['pizzasToPromo']);
+            // For each one of the cells
+            foreach($pizzaToResetJson as $pizzasId){
+                // Get the choosen pizza
+                $pizzaToSetAgain = $repo->findPizzaById($pizzasId);
+                // Set it to "PROMO"
+                $pizzaToSetAgain->setType("PROMO");
+                // Persist it
+                $manager->persist($pizzaToSetAgain);
+            }
+            // Flush it twice
+            $manager->flush();
+        }
+    
+        // Flash
+        $this->addFlash(
+            "success",
+            "Les pizzas en promotions ont bien été modifiées !"
+        );
+
+        return $this->render('admin/pizza/promo.html.twig', [
+            'myForm' => $form->createView(),
+            'pizzas' => $pizzas,
+        ]);
+    }
     
       
     /**
--- src/Form/AdminPromoType.php
+++ src/Form/AdminPromoType.php
@@ -9,11 +9,15 @@ use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+/**
+ * Form dedicated to change the pizza's type.
+ */
 class AdminPromoType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+        // A JSON array given in order to succeed the treatment in PHP
         ->add('pizzasToPromo', HiddenType::class);
     }
 
