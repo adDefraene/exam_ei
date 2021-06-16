11421c580b171a70758c0c0c1890979bc2a58cb6
Adrien Defraene <adriendefraene@gmail.com>
2021-06-16 07:40:59 +0200

    Fixed errors in order treatment

--- src/Form/OrderType.php
+++ src/Form/OrderType.php
@@ -16,7 +16,10 @@ class OrderType extends ApplicationType
     {
         $builder
             ->add('date', DateTimeType::class)
-            ->add('orderItemsJson', HiddenType::class)
+            ->add('orderItemsJson', HiddenType::class, [
+                'data_class' => null,
+                'mapped' => false
+            ])
             ->add('ifDelivered', HiddenType::class)
         ;
     }
